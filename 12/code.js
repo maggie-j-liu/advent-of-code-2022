f42376f686ec70d6f983d497d2507a98dee6edff
Maggie Liu <maggie.j.liu@gmail.com>
2022-12-11 21:40:55 -0800

    neighbors util function

--- 12/code.js
+++ 12/code.js
@@ -1,17 +1,13 @@
 import getInput from "../utils/getInput.js";
-import { letterToIdx } from "../utils/index.js";
+import { letterToIdx, neighbors } from "../utils/index.js";
 
 const input = await getInput(import.meta.url);
 const lines = input.split("\n");
-const MAX_J = lines[0].length;
-const MAX_I = lines.length;
+const ROWS = lines.length;
+const COLS = lines[0].length;
 const map = [];
 const gsteps = [];
 let end;
-
-const inBounds = (i, j) => i >= 0 && i < MAX_I && j >= 0 && j < MAX_J;
-const di = [-1, 0, 0, 1];
-const dj = [0, 1, -1, 0];
 
 const pt1 = [];
 const pt2 = [];
@@ -42,14 +38,8 @@ const bfs = (q) => {
     if (i === end.i && j === end.j) {
       return steps;
     }
-    for (let d = 0; d < 4; d++) {
-      let ni = i + di[d];
-      let nj = j + dj[d];
-      if (
-        inBounds(ni, nj) &&
-        map[ni][nj] <= map[i][j] + 1 &&
-        gsteps[ni][nj] > steps + 1
-      ) {
+    for (const [ni, nj] of neighbors(i, j, ROWS, COLS)) {
+      if (map[ni][nj] <= map[i][j] + 1 && gsteps[ni][nj] > steps + 1) {
         gsteps[ni][nj] = steps + 1;
         q.push({ i: ni, j: nj, steps: steps + 1 });
       }
--- utils/index.js
+++ utils/index.js
@@ -14,3 +14,18 @@ export const letterToIdx = (letter) => {
     upper: true,
   };
 };
+
+export const neighbors = (i, j, rows, cols) => {
+  const di = [-1, 0, 1, 0];
+  const dj = [0, 1, 0, -1];
+  const inBounds = (_i, _j) => _i >= 0 && _i < rows && _j >= 0 && _j < cols;
+  const nlist = [];
+  for (let d = 0; d < 4; d++) {
+    const ni = i + di[d];
+    const nj = j + dj[d];
+    if (inBounds(ni, nj)) {
+      nlist.push([ni, nj]);
+    }
+  }
+  return nlist;
+};
